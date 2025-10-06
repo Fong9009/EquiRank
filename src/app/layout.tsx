548865e911206166e2772b15615b9f58f29cfbcc
Fong <chon0012@student.monash.edu>
2025-10-06 21:38:47 +1100

    Implement CSP compliance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import Navbar from '@/components/layout/Navbar'
+import Script from 'next/script'
+import { headers } from 'next/headers'
 import { NextAuthProvider } from '@/components/providers/NextAuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,16 +18,26 @@ export const metadata: Metadata = {
   },
 }
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 })
 {
+  const h = await headers();
+  const nonce = h.get('x-nonce') || undefined;
   return (
     <html lang="en" className="dark">
       <body className={inter.className}>
         <NextAuthProvider>
+          <Script
+            id="recaptcha-api"
+            src="https://www.google.com/recaptcha/api.js"
+            strategy="afterInteractive"
+            nonce={nonce}
+            async
+            defer
+          />
           <Navbar />
           <main>
             {children}
