d9b483afdabdbf08fcb1da47f01b426bbe3f1cee
Chay Fong Hong <chayfong9009@gmail.com>
2026-02-19 23:43:14 +0700

    Set home page to use dynamic rendering due to SessionProvider context

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,9 @@ import Features from "@/components/pages/home/Features";
 import Benefits from "@/components/pages/home/Benefits";
 import Footer from "@/components/layout/Footer";
 
+// Avoid prerender: home page uses SessionProvider/useSession (client context)
+export const dynamic = 'force-dynamic';
+
 export default function Home() {
   return (
       <>
